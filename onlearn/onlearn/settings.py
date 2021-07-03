ec3639f98e13be3df99da95e69478026abf106b8
sawad kk <60354165+sawadkk@users.noreply.github.com>
2021-07-03 19:48:58 +0530

    Update settings.py

--- onlearn/onlearn/settings.py
+++ onlearn/onlearn/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2e$25z9nml18*rk$bo-ljnbjk)_#*6y$+jd18kk6g!9qf28t1y'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
